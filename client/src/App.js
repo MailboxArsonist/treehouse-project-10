79dce5ff45693a2c8a101732647a823afb7e86f3
Matthew Dodd <matthew.dodd93@gmail.com>
2019-02-27 19:27:07 +0100

    Imported router and set up routes for courses and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
+
+//import components 
+import Nav from './components/Nav';
+import Courses from './components/Courses';
+import NotFound from './components/NotFound';
+import CourseDetail from './components/CourseDetail/CourseDetail';
+
+
 class App extends Component {
+  state = {
+    loading : false,
+    courses : []
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Router>
+          <>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Courses}/>
+              <Route path="/courses/:id" component={CourseDetail}/>
+              <Route component={NotFound}/>
+            </Switch>
+          </>
+        </Router>
       </div>
     );
   }
