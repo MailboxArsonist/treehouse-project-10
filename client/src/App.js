ad2aaeb977287034e623e466ed61c46dc3724795
Matthew Dodd <matthew.dodd93@gmail.com>
2019-02-28 14:47:30 +0100

    Added new components and renders them on the correct routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,13 @@ import './App.css';
 //import components 
 import Nav from './components/Nav';
 import Courses from './components/Courses';
-import NotFound from './components/NotFound';
+import NotFound from './components/Errors/NotFound';
 import CourseDetail from './components/CourseDetail/CourseDetail';
+import UserSignUp from './components/User/UserSignUp';
+import UserSignIn from './components/User/UserSignIn';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/CourseDetail/UpdateCourse';
+
 
 
 class App extends Component {
@@ -23,7 +28,11 @@ class App extends Component {
             <Nav />
             <Switch>
               <Route exact path="/" component={Courses}/>
-              <Route path="/courses/:id" component={CourseDetail}/>
+              <Route exact path="/create" component={CreateCourse}/>
+              <Route exact path="/courses/:id" component={CourseDetail}/>
+              <Route exact path="/courses/:id/update" component={UpdateCourse}/>
+              <Route exact path="/signup" component={UserSignUp}/>
+              <Route exact path="/signin" component={UserSignIn}/>
               <Route component={NotFound}/>
             </Switch>
           </>
