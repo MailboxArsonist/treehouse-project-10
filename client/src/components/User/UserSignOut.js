3f2d54104b2fe4dae4e41f3a400c6d90a6a3a9df
Matthew Dodd <matthew.dodd93@gmail.com>
2019-02-28 14:48:23 +0100

    Components for signup/in/out - render correctly, need to add functionality

---
+++ client/src/components/User/UserSignOut.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class UserSignOut extends Component {
+    render(){
+        return(
+            <h1>You have been Signed out</h1>
+        )
+    }
+}
+
+export default UserSignOut;
