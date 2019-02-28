716f3beeacb95f4526d998002aa8b300dcbb6934
Matthew Dodd <matthew.dodd93@gmail.com>
2019-02-28 14:46:12 +0100

    Created a Forbidden component, all errors now go in the Error directory

---
+++ client/src/components/Errors/Forbidden.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forbidden = () => {
+    return (
+        <>
+            <h1>Access Denied</h1>
+            <p>The email/username you provided was incorrect</p>
+        </>
+    )
+}
+
+export default Forbidden;
