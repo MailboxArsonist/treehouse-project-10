b8fa056dab0dba498fccf5a157cf3cfad47860f9
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-01 09:36:26 +0100

    Can now log in and signup, need to add client side validation for both

--- client/src/components/User/UserSignIn.js
+++ client/src/components/User/UserSignIn.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 class UserSignIn extends Component {
+    state = {
+        emailAddress: '',
+        password: '',
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {emailAddress, password} = this.state;
+        if(emailAddress.length > 0 && password.length > 0){
+            //all good, make the request
+            axios({
+                method: 'get',
+                url : 'http://localhost:5000/api/users',
+                auth: {
+                username: emailAddress,
+                password: password
+              }})
+              .then(res => {
+                  if(res.status === 200 || res.status === 304){
+                      const name = res.data.firstName
+                      this.props.authenticateUser(emailAddress, password, name, true);
+                  }
+              })
+              .catch(err => {
+                  //come back to here and add in the 
+                  if(err.response.status === 403){
+                    this.props.authenticateUser();
+                  }
+                console.log(err.response);
+              });
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+
     render(){
+        console.log('rendered')
         return(
             <div className="bounds">
                 <div className="grid-33 centered signin">
                     <h1>Sign In</h1>
                     <div>
-                        <form>
-                            <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
-                            <div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
+                        <form onSubmit={this.handleSubmit}>
+                            <div><input onChange={this.handleChange} id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" /></div>
+                            <div><input onChange={this.handleChange} id="password" name="password" type="password" className="" placeholder="Password" /></div>
                             <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary">Cancel</button></div>
                         </form>
                     </div>
--- client/src/components/User/UserSignUp.js
+++ client/src/components/User/UserSignUp.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 class UserSignUp extends Component {
+    state = {
+        firstName: '',
+        lastName: '',
+        emailAddress: '',
+        password: '',
+        confirmPassword: ''
+    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {emailAddress, password, firstName, lastName, confirmPassword} = this.state;
+        if(emailAddress !== '' && password !== '' && firstName !== '' && lastName !== '' && password === confirmPassword){
+            //all good, make the request
+            axios.post('http://localhost:5000/api/users', {
+                firstName,
+                lastName,
+                emailAddress,
+                password
+            })
+              .then(res => {
+                  console.log(res)
+              })
+              .catch(err => {
+                console.log(err.response);
+              });
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
     render(){
+        console.log('rendered')
         return(
             <div className="bounds">
                 <div className="grid-33 centered signin">
                     <h1>Sign Up</h1>
                     <div>
-                        <form>
-                            <div><input id="firstName" name="firstName" type="text" className="" placeholder="First Name" value="" /></div>
-                            <div><input id="lastName" name="lastName" type="text" className="" placeholder="Last Name" value="" /></div>
-                            <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
-                            <div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
-                            <div><input id="confirmPassword" name="confirmPassword" type="password" className="" placeholder="Confirm Password" value="" /></div>
-                            <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign Up</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
+                        <form onSubmit={this.handleSubmit}>
+                            <div><input onChange={this.handleChange} id="firstName" name="firstName" type="text" className="" placeholder="First Name" /></div>
+                            <div><input onChange={this.handleChange} id="lastName" name="lastName" type="text" className="" placeholder="Last Name" /></div>
+                            <div><input onChange={this.handleChange} id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" /></div>
+                            <div><input onChange={this.handleChange} id="password" name="password" type="password" className="" placeholder="Password" /></div>
+                            <div><input onChange={this.handleChange} id="confirmPassword" name="confirmPassword" type="password" className="" placeholder="Confirm Password" /></div>
+                            <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign Up</button><button className="button button-secondary">Cancel</button></div>
                         </form>
                     </div>
                     <p>&nbsp;</p>
