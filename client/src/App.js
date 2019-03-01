3fb489edbee11286dce696cc64f6a4b2269163e6
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-01 09:33:59 +0100

    Modified sign in method to update to a name property, passed to Nav component to display user name

--- client/src/App.js
+++ client/src/App.js
@@ -12,27 +12,54 @@ import UserSignUp from './components/User/UserSignUp';
 import UserSignIn from './components/User/UserSignIn';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/CourseDetail/UpdateCourse';
+import Forbidden from './components/Errors/Forbidden';
 
 
 
 class App extends Component {
   state = {
-    loading : false,
-    courses : []
+    authenticated : null,
+    username : null,
+    password : null,
+    name : null
   }
+
+  signIn = (username, password, name, authenticated) => {
+    if(authenticated){
+      this.setState({
+        authenticated,
+        username,
+        name,
+        password
+      })
+    } else {
+      this.setState({
+        authenticated : false,
+        username : null,
+        password : null
+      })
+    }
+    
+  }
+  
+  
   render() {
+
     return (
       <div>
         <Router>
           <>
-            <Nav />
+            <Nav name={this.state.name}/>
+            {this.state.authenticated === false && <Forbidden />}
             <Switch>
               <Route exact path="/" component={Courses}/>
               <Route exact path="/create" component={CreateCourse}/>
               <Route exact path="/courses/:id" component={CourseDetail}/>
               <Route exact path="/courses/:id/update" component={UpdateCourse}/>
               <Route exact path="/signup" component={UserSignUp}/>
-              <Route exact path="/signin" component={UserSignIn}/>
+              <Route exact path="/signin" render={props => (
+                <UserSignIn authenticateUser={this.signIn}/>
+              )}/>
               <Route component={NotFound}/>
             </Switch>
           </>
