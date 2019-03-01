f9456fa873267fa432a26711d1bb05c4890df76a
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-01 09:34:51 +0100

    Conditional rendering added to display username & signout or signin & signup

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-const Nav = () => {
+const Nav = (props) => {
+
     return (
       <div className="header">
         <div className="bounds">
           <h1 className="header--logo">Courses</h1>
-          <nav><Link className="signup" to="/signup">Sign Up</Link><Link className="signin" to="signin">Sign In</Link></nav>
+          <nav>{props.name ?
+                <>
+                  <p>Welcome {props.name}</p>
+                  <Link className="signout" to="/signout">Sign Out</Link>
+                </>  
+                :
+                <>
+                  <Link className="signup" to="/signup">Sign Up</Link>
+                  <Link className="signin" to="/signin">Sign In</Link>
+                </>
+                }</nav>
         </div>
       </div>
     )
