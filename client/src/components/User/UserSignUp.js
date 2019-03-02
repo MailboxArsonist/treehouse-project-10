cddea3301c37cb0553496f3eb771763b0e910129
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-02 09:07:33 +0100

    Functionality added to deal with any conflicting user email

--- client/src/components/User/UserSignUp.js
+++ client/src/components/User/UserSignUp.js
@@ -9,8 +9,15 @@ class UserSignUp extends Component {
         lastName: '',
         emailAddress: '',
         password: '',
-        confirmPassword: ''
+        confirmPassword: '',
+        alreadyExists: false,
+        validFirstName: null,
+        validLastName: null,
+        validEmail: null,
+        validPassword: null,
     }
+
+    //handles submit of form
     handleSubmit = (e) => {
         e.preventDefault();
         const {emailAddress, password, firstName, lastName, confirmPassword} = this.state;
@@ -23,29 +30,55 @@ class UserSignUp extends Component {
                 password
             })
               .then(res => {
-                  console.log(res)
+                  this.props.authenticateUser(emailAddress, password, firstName, true );
               })
               .catch(err => {
+                  //check if error is conflict(already existing user)
+                  if(err.response.status === 409){
+                      //conflict, display error
+                      this.setState({alreadyExists: true});
+                  }
                 console.log(err.response);
               });
         }
     }
 
+    checkIfExists = () => {
+        if(this.state.alreadyExists){
+            return(
+                <div className="validation--duplicate">Looks like you already have an account:<Link to="/signin"> Sign In</Link></div>
+            );
+        } else {
+            return null;
+        }
+    }
+    //handles interaction on inputs, updates the state that matches the input name
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
-        })
+        });
     }
+    //check for a valid email address
+    isValidEmail = (email) => {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+    //check for firstname & lastname
+    isValidName = (name) => {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(name);
+    }
+    //check for a valid password
 
     render(){
-        console.log('rendered')
+        console.log('rendered signup')
         return(
             <div className="bounds">
                 <div className="grid-33 centered signin">
                     <h1>Sign Up</h1>
                     <div>
                         <form onSubmit={this.handleSubmit}>
-                            <div><input onChange={this.handleChange} id="firstName" name="firstName" type="text" className="" placeholder="First Name" /></div>
+                            <div>{this.checkIfExists()}<input onChange={this.handleChange} id="firstName" name="firstName" type="text" className="" placeholder="First Name" /></div>
                             <div><input onChange={this.handleChange} id="lastName" name="lastName" type="text" className="" placeholder="Last Name" /></div>
                             <div><input onChange={this.handleChange} id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" /></div>
                             <div><input onChange={this.handleChange} id="password" name="password" type="password" className="" placeholder="Password" /></div>
