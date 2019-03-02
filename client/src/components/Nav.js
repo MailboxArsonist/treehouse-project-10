469b7a02b476517feef7481bade1b0d525947462
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-02 09:06:46 +0100

    Fixed error to correctly show signout & name

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,16 +2,16 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Nav = (props) => {
-
+  console.log('rendered nav')
     return (
       <div className="header">
         <div className="bounds">
           <h1 className="header--logo">Courses</h1>
           <nav>{props.name ?
-                <>
-                  <p>Welcome {props.name}</p>
-                  <Link className="signout" to="/signout">Sign Out</Link>
-                </>  
+                <div>
+                  <span>Welcome {props.name}</span>
+                  <Link className="signout" to="/" onClick={props.signOut}>Sign Out</Link>
+                </div>  
                 :
                 <>
                   <Link className="signup" to="/signup">Sign Up</Link>
