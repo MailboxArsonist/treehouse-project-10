ba2b6c10794b67ca62b7144bc26c3c33c68ace21
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-03 17:55:17 +0100

    Added some clientside validation, just for empty string on inputs. Also renders an error message from API when user info is incorrect.

--- client/src/components/User/UserSignIn.js
+++ client/src/components/User/UserSignIn.js
@@ -1,67 +1,109 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 class UserSignIn extends Component {
-    state = {
-        emailAddress: '',
-        password: '',
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        const {emailAddress, password} = this.state;
-        if(emailAddress.length > 0 && password.length > 0){
-            //all good, make the request
-            axios({
-                method: 'get',
-                url : 'http://localhost:5000/api/users',
-                auth: {
-                username: emailAddress,
-                password: password
-              }})
-              .then(res => {
-                  if(res.status === 200 || res.status === 304){
-                      const name = res.data.firstName
-                      this.props.authenticateUser(emailAddress, password, name, true);
-                  }
-              })
-              .catch(err => {
-                  //come back to here and add in the 
-                  if(err.response.status === 403){
-                    this.props.authenticateUser();
-                  }
-                console.log(err.response);
-              });
+	state = {
+		emailAddress: "",
+		password: "",
+		attemptedSignIn: false,
+		accessDenied: false,
+		accessDeniedMessage : ''
+		
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+		const { emailAddress, password } = this.state;
+		if (emailAddress.length > 0 && password.length > 0) {
+			//all good, make the request
+			axios.get("http://localhost:5000/api/users", {
+					auth: {
+						username: emailAddress,
+						password: password
+					}
+				})
+				.then(res => {
+					if (res.status === 200 || res.status === 304) {
+						const name = res.data[0].firstName;
+						this.props.authenticateUser(emailAddress, password, name, true);
+					}
+				})
+				.catch(err => {
+					//come back to here and add in the
+					if (err.response.status === 403) {
+						this.setState({
+							accessDenied : true,
+							accessDeniedMessage : err.response.data.accessDenied
+						});
+					}
+				});
+		} else {
+			this.setState({attemptedSignIn: true});
+		}
+	};
+	//handles changes to the input, set state = to input value. Reset access denied to remove any error message
+	handleChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+			accessDenied : false
+		});
+	};
+
+	//Will return an error message if the user tries to submit the form without all inputs correctly filled out
+    errorMessage = (name, errorMessage) => {
+        if(this.state.attemptedSignIn && this.state[name] === ''){
+            //form has been submitted and this input was incorrectly filled in.
+            return <label>{errorMessage}</label>
+        } else {
+            return null;
         }
     }
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-
-
-    render(){
-        console.log('rendered')
-        return(
-            <div className="bounds">
-                <div className="grid-33 centered signin">
-                    <h1>Sign In</h1>
-                    <div>
-                        <form onSubmit={this.handleSubmit}>
-                            <div><input onChange={this.handleChange} id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" /></div>
-                            <div><input onChange={this.handleChange} id="password" name="password" type="password" className="" placeholder="Password" /></div>
-                            <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary">Cancel</button></div>
-                        </form>
-                    </div>
-                    <p>&nbsp;</p>
-                    <p>Don't have a user account? <Link to="/signup">Click here</Link> to sign up!</p>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		console.log("rendered signin");
+		return (
+			<div className="bounds">
+				<div className="grid-33 centered signin">
+					<h1>Sign In</h1>
+					<div>
+						<form onSubmit={this.handleSubmit}>
+							<div>
+								{this.state.accessDenied && <label>{this.state.accessDeniedMessage}</label>}
+								{this.errorMessage('emailAddress', 'You must enter your email address')}
+								<input
+									onChange={this.handleChange}
+									id="emailAddress"
+									name="emailAddress"
+									type="text"
+									className=""
+									placeholder="Email Address"
+								/>
+							</div>
+							<div>
+								{this.errorMessage('password', 'You must enter your password')}
+								<input
+									onChange={this.handleChange}
+									id="password"
+									name="password"
+									type="password"
+									className=""
+									placeholder="Password"
+								/>
+							</div>
+							<div className="grid-100 pad-bottom">
+								<button className="button" type="submit">
+									Sign In
+								</button>
+								<button className="button button-secondary">Cancel</button>
+							</div>
+						</form>
+					</div>
+					<p>&nbsp;</p>
+					<p>Don't have a user account? <Link to="/signup">Click here</Link> to sign up!</p>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default UserSignIn;
