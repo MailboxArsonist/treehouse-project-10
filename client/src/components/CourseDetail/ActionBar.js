840269439288f1f62b6a80e127ee1cec89272ad1
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-05 22:55:05 +0100

    Modified to recieve context instead of props from app

--- client/src/components/CourseDetail/ActionBar.js
+++ client/src/components/CourseDetail/ActionBar.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import {UserContext} from '../../context/UserProvider';
 
 const ActionBar = (props) => {
+    console.log('rendered actionbar')
+    const {userId} = useContext(UserContext);
+    console.log(userId);
+    console.log(props.userId)
+    //what i need to do
+    //1. Check to see if user Id from aut user matches user id from courseDetail
+    //If they don't match, dont render update and delete
     return (
         <div className="actions--bar">
             <div className="bounds">
                 <div className="grid-100">
                     <span>
-                        <Link className="button" to={`/courses/${props.courseId}/update`}>Update Course</Link>
-                        <Link className="button" to="#">Delete Course</Link>
+                        {userId === props.courseUserId && (
+                            <>
+                                <Link className="button" to={`/courses/${props.courseId}/update`}>Update Course</Link>
+                                <Link className="button" to="#">Delete Course</Link>
+                            </>
+                        )}
+                        
                     </span>
                     <Link className="button button-secondary" to="/">Return to List</Link>
                 </div>
--- client/src/components/CourseDetail/CourseDetail.js
+++ client/src/components/CourseDetail/CourseDetail.js
@@ -25,12 +25,13 @@ class CourseDetail extends Component {
               .catch(err => console.log(err))
       }
       render () {
+        console.log('course detail rendered')
           return (
             <div>
-                <ActionBar courseId={this.state.course._id}/>
                 <div className="bounds course--detail">
                   {this.state.loading ? <h1>Loading</h1> : 
                     <>
+                    <ActionBar courseId={this.state.course._id} courseUserId={this.state.course.user._id}/>
                     <CourseContent 
                         courseTitle={this.state.course.title} 
                         courseInstructor={`${this.state.course.user.firstName} ${this.state.course.user.lastName}`} 
