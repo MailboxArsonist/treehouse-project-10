7dc411437f37791d80cfec2ca02a63e10e6eb15a
Matthew Dodd <matthew.dodd93@gmail.com>
2019-03-12 13:59:11 +0100

    Removed the link and modified the h1 text

--- client/src/components/Errors/Forbidden.js
+++ client/src/components/Errors/Forbidden.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Forbidden = () => {
     return (
         <div className="error-block">
-            <h1>Hey, you there!</h1>
+            <h1>Hey, you there, you don't have access to this page!</h1>
             <p>You need to be signed in to view this page</p>
-            <Link to="/signin">Sign in</Link>
         </div>
     )
 }
